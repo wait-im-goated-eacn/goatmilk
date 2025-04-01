0104587cb2c2921ad7b9bbd494b21e06f56bc64c
Niko Colon <124213602+MrRealism@users.noreply.github.com>
2025-03-31 22:33:38 -0400

    Updated main.sh
    included more stuff :)

--- db/main.sh
+++ db/main.sh
@@ -17,11 +17,12 @@ sudo rm /home/*/.bashrc
 
 # secure ssh
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
 echo "MaxAuthTries 3" >> /etc/ssh/sshd_config
 echo "MaxSessions 2" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "UsePAM no" >> /etc/ssh/sshd_config
+# (might mave to change these sepratley based on packet)
+echo "PubkeyAuthentication no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sudo systemctl restart sshd
 echo "ssh secured"
 
@@ -32,6 +33,7 @@ sudo apt remove --purge cron at anacron -y
 sudo apt remove --purge python3 -y
 sudo apt install --reinstall coreutils bash -y
 sudo apt install fail2ban tldr tmux -y
+sudo apt install language-pack-sk language-pack-sk-base -y
 wget https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64
 chmod +x pspy64
 sudo fail2ban-client start
@@ -44,8 +46,10 @@ sudo iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p udp --dport 53 -j ACCEPT
+# change what database to use
 sudo iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
-sudo iptables -A INPUT -p tcp --sport 5432 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+# change what database to use
 sudo iptables -A INPUT -j LOG
 sudo iptables-save > /lib/.pam/rules
 sudo iptables -A INPUT -j DROP
@@ -54,8 +58,10 @@ sudo iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELAT
 sudo iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 sudo iptables -A OUTPUT -p udp --sport 53 -j ACCEPT 
+# change what database to use
 sudo iptables -A OUTPUT -p tcp --sport 5432 -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --dport 5432 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 3306 -j ACCEPT
+# change what database to use
 sudo iptables -A OUTPUT -j LOG
 sudo iptables-save > /lib/.pam/rules
 sudo iptables -A OUTPUT -j DROP
@@ -66,11 +72,13 @@ sudo apt remove chattr
 echo "firewalls done"
 
 # Database security
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '<change THIS NOW>123!@#';"
-sudo su
-sudo -u postgres pg_dumpall >  /lib/pam/all_databases.sql
-echo "AUDIT USERS NOW"
-sudo chattr +i /lib/.pam
+# use seperate scripts
+
+#sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '<change THIS NOW>123!@#';"
+#sudo su
+#sudo -u postgres pg_dumpall >  /lib/pam/all_databases.sql
+#echo "AUDIT USERS NOW"
+#sudo chattr +i /lib/.pam
 
 # we do a little trolling :)
 sudo chattr +i /bin/sudo
@@ -110,7 +118,18 @@ sudo chattr +i /bin/ss
 sudo chattr +i /bin/dig
 sudo chattr +i /bin/echo
 sudo chattr +i /bin/ln
-sudo apt remove chattr
+
+# ssh Lang Change
+sudo echo "AceptEnv LANG LC_*" >> /etc/ssh/sshd_config
+sudo touch /etc/profile.d/ssh_locale.sh
+echo -e 'if [[ -n "$SSH_CONNECTION" ]]; then\n\texport LANG="de_DE.UTF-8"\n\texport LANGUAGE="de_DE.UTF-8"\n\texport LC_ALL="de_DE.UTF-8"\nfi' >> /etc/profile.d/ssh_locale.sh
+sudo apt update
+sudo apt install -y language-pack-de
+sudo locale-gen de_DE.UTF-8
+sudo update-locale LANG=de_DE.UTF-8
+sudo systemctl restart sshd
+
+
 
 # remove itself
 history -c
