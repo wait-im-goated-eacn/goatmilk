bbf66d907eaa69e6de1ac8effd315e2427155e4f
ap0kalypse <129468267+LunarKalypse@users.noreply.github.com>
2025-04-11 21:48:31 -0400

    Update packages.sh

--- dns/packages.sh
+++ dns/packages.sh
@@ -1,5 +1,7 @@
 yum install -y iptables
 yum install -y rsync
 dnf remove -y cronie at anacron
+dnf install -y tmux git
 wget -P ~ https://github.com/DominicBreuker/pspy/releases/download/v1.2.2/pspy64
 - chmod ~/pspy64 +x
+yum install -y inotify-tools
