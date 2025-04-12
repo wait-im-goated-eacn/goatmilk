64610c986915df7c0099e28df32beaec975e11b2
CWiendl <cwiendl@icloud.com>
2025-04-11 20:36:26 -0400

    gettign smb

--- FTP-SSH/General/UserDel_and_Add.sh
+++ FTP-SSH/General/UserDel_and_Add.sh
@@ -27,6 +27,31 @@ done
 # List of users to re-add. 
 # REPLACE IF NEEDED
 users=(
+    benjamin_franklin
+    alexander_hamilton
+    john_adams
+    theodore_roosevelt
+    franklin_d
+    winston_churchill
+    florence_nightingale
+    eleanor_roosevelt
+    mother_teresa
+    mahatma_gandhi
+    socrates
+    plato
+    aristotle
+    hippocrates
+    archimedes
+    rene_descartes
+    voltaire
+    jean_jacques_rousseau
+    immanuel_kant
+    friedrich_nietzsche
+    sigmund_freud
+    charles_darwin
+    marie_antoinette
+    louis_xiv
+    peter_the_great
     camille_jenatzy
     gaston_chasseloup
     leon_serpollet
